dbf3ad2e18f10f85a80f9cbfbf6974c135b313b9
Sky <skydebreuil@gmail.com>
2016-04-02 15:01:50 +0900

    starting neural network test

--- Assets/DATA_Point.cs
+++ Assets/DATA_Point.cs
@@ -1,10 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum PointType
+{
+	Red,
+	Blue
+}
+
 public class DATA_Point : MonoBehaviour 
 {
+	public PointType Type;
+
+	public void OnValidate()
+	{
+		var renderer = GetComponent<SpriteRenderer>();
+
+		if(renderer)
+		{
+			switch (Type)
+			{
+			case PointType.Red:
+				renderer.color = Color.red;
+				break;
+			case PointType.Blue:
+				renderer.color = Color.blue;
+				break;
+			}
+		}
+	}
+
 	public void OnDrawGizmos()
 	{
+		
 //		Gizmos.drawc
 	}
 }
---
+++ Assets/SCR_Neuron.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Connection : MonoBehaviour
+{
+	public SCR_Neuron Source;
+	public SCR_Neuron Target;
+}
+
+public class SCR_Neuron : MonoBehaviour
+{
+	public int Feature;
+	public int Layer;
+
+	public float Weight;
+	public float Bias;
+
+	public SCR_Neuron[] Sources = new SCR_Neuron[0];
+	public SCR_Neuron[] Targets = new SCR_Neuron[0];
+
+	public void Awake()
+	{
+		var all = SCR_Neuron.FindObjectsOfType<SCR_Neuron>();
+
+		Sources = all.Where(_ => _.Layer == Layer - 1).ToArray();
+		Targets = all.Where(_ => _.Layer == Layer + 1).ToArray();
+	}
+
+	public void OnDrawGizmos()
+	{
+		var x = Layer;
+		var y = Feature;
+	}
+}
