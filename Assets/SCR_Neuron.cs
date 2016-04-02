75658b57e36f71895f3bd623acd4ca2289c4a4de
Sky <skydebreuil@gmail.com>
2016-04-02 15:14:53 +0900

    neural net start

--- Assets/SCR_Neuron.cs
+++ Assets/SCR_Neuron.cs
@@ -13,23 +13,51 @@ public class SCR_Neuron : MonoBehaviour
 	public int Feature;
 	public int Layer;
 
-	public float Weight;
 	public float Bias;
+	public float[] Weights;
 
 	public SCR_Neuron[] Sources = new SCR_Neuron[0];
 	public SCR_Neuron[] Targets = new SCR_Neuron[0];
 
 	public void Awake()
 	{
+		Refresh();
+	}
+
+	public void Refresh()
+	{
 		var all = SCR_Neuron.FindObjectsOfType<SCR_Neuron>();
 
 		Sources = all.Where(_ => _.Layer == Layer - 1).ToArray();
 		Targets = all.Where(_ => _.Layer == Layer + 1).ToArray();
+
+		Weights = new float[Weights.Length];
+
+		var x = Layer * 2 + 1;
+		var y = -Feature - 2;
+
+		transform.position = new Vector2(x, y);
+	}
+
+	public void OnValidate()
+	{
+		if(Application.isPlaying)
+		{
+			return;
+		}
+
+		Refresh();
 	}
 
 	public void OnDrawGizmos()
 	{
-		var x = Layer;
-		var y = Feature;
+		Gizmos.color = Color.yellow;
+
+//		Gizmos.DrawWireSphere(transform.position, 0.25F);
+
+		foreach (var item in Targets)
+		{
+			Gizmos.DrawLine(transform.position, item.transform.position);
+		}
 	}
 }
